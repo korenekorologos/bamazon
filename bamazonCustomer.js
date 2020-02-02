ad3c8125c09c07e0ec209357d2b40785c5e3a0f1
korene korologos <korenekorologos@gmail.com>
2020-02-02 11:03:51 -0700

    bamazonCustomer.js has been updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,98 +1,121 @@
 //connects our required packages
 var mysql = require("mysql"); 
-var inquirer = ("inquirer"); 
-var table = require("cli-table2");
+var inquirer = require("inquirer"); 
+var tables = require("cli-table2");
 
-//have the connection variable connect to mysql database
+
+//connection variable that connects to mysql database 
 var connection = mysql.createConnection({
     host: "localhost",
-    user: "", //will want to leave blank**
-    password: "", //will want to leave blank**
-    database: "bamazon_db", //schema name in mysql
+    user: "root",
+    password: "",
+    database: "bamazon_db",
     port: 3306
-}); 
-
-//calls it 
-connection.connect(function(err){
-  if (err) throw err;
-});
-
-var display = function() {
-  connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-      console.table(results);
-  })
-};
-
-var run = function() {
-  //all products available for purchase in the query database 
-  connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-      //prompt the user for which they'd like to purchase, once you have the products
-      inquirer.prompt([
-          {
-              name: "product",
-              type: "list",
-              choices: function() {
-                  var choiceArray = [];
-                  for (var i = 0; i < results.length; i++) {
-                      choiceArray.push(results[i].product_name);
-                  }
-                  return choiceArray;
-              },
-              message: "What are you looking to purchase?"
-          },
-          {
-              name: "amount",
-              type: "input",
-              message: "How many would you like to buy?"
-          }
-      ]).then(function(answer) {
-          var chosenProduct;
-          for (var i = 0; i < results.length; i++) {
-              if (results[i].product_name === answer.product) {
-                  chosenProduct = results[i];
-              }
-          }
-
-          if (chosenProduct.stock_quantity > parseInt(answer.amount)) {
-              connection.query("UPDATE products SET ? WHERE ?", [
-              {
-                  stock_quantity: chosenProduct.stock_quantity - parseInt(answer.amount)
-              },
-              {
-                  id: chosenProduct.id
-              }], function(error) {
-                  if (error) throw err;
-                  console.log("\n\n");
-                  console.log("==============================================");
-                  console.log("Congrats, your product was purchased successfully!");
-                  console.log("==============================================");
-                  console.log("Here's your purchase summary");
-                  console.log("-----------------------------");
-                  console.log("Item Name: " +  chosenProduct.product_name);
-                  console.log("Item Count: " + parseInt(answer.amount));
-                  console.log("-----------------------------");
-                  console.log("Total: " + "$" + (chosenProduct.price * parseInt(answer.amount)));
-                  console.log("==============================================");
-                  console.log("\n\n");
-                  display();
-                  run();
-              })
-          } else {
-              console.log("==============================================");
-              console.log("Insufficient stock.");
-              console.log("==============================================");
-              display();
-              run();
-          }
-      });
   });
-};
+  
+  //calls it
+  connection.connect();
+  
+  //use cli-table2 package to display the data table from mysql 
+  var display = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log("-----------------------------");  //the decoration 
+      console.log("Hey There, Welcome To Bamazon!");
+      console.log("-----------------------------");
+      console.log("");
+      console.log("Check out our products list bellow...");
+      console.log("");
 
-display();
-run();
-
-
-
-
+      //our table variable that connects to the cli table 
+      var table = new tables({
+        head: ["Product Id", "Product Description", "Cost"],
+        colWidths: [12, 50, 8],
+        colAligns: ["center", "left", "right"],
+        style: {
+          head: ["aqua"],
+          compact: true
+        }
+      });
+  
+  //for loop that uses the length of the result and then pushes the product name and price 
+      for (var i = 0; i < res.length; i++) {
+        table.push([res[i].id, res[i].products_name, res[i].price]);
+      }
+  
+      console.log(table.toString());
+      console.log("");
+    //  shopping();
+    }); 
+  };
+  
+  //inquirer will prompt the user through a series of questions determine what product they would like 
+     //to buy and also the quanity of the item  
+  var shopping = function() {
+    inquirer
+      .prompt({
+        name: "productToBuy",
+        type: "input",
+        message: "Enter the Product Id of the item you wish to buy."
+      })
+      .then(function(answer1) {
+        var selection = answer1.productToBuy;
+        connection.query("SELECT * FROM products WHERE Id=?", selection, function(
+       //this^ connects the query to the database 
+          err,
+          res
+        ) {
+          if (err) throw err;  //throws error if they slect something that is not on the poduct list 
+          if (res.length === 0) {
+            console.log(
+              "The product entered does not exist. Please choose a product id from the list above"
+            );
+              //calls it 
+            shopping();
+          } else {
+            //builds the quanity input
+            inquirer
+              .prompt({
+                name: "quantity",
+                type: "input",
+                message: "How many items woul you like to purchase?"
+              })
+              .then(function(answer2) {
+                var quantity = answer2.quantity;
+                if (quantity > res[0].stock_quantity) {
+                  console.log(
+                    "It looks like we only have " +
+                      res[0].stock_quantity +
+                      " items of the product selected"
+                  );
+                  //calls it 
+                  shopping();
+                } else {
+                  console.log("");
+                  console.log(res[0].products_name + " purchased");
+                  console.log(quantity + " qty @ $" + res[0].price);  //consoles out the name, price and quanity 
+  
+                  //variable will update the stop quanity in the database 
+                  var newQuantity = res[0].stock_quantity - quantity;
+                  connection.query(
+                    "UPDATE products SET stock_quantity = " +
+                      newQuantity +
+                      " WHERE id = " +
+                      res[0].id,
+                    function(err, resUpdate) {
+                      if (err) throw err;
+                      console.log("");
+                      console.log("Congrats, your order has been processed");
+                      console.log("Thanks so much for shopping with us!");
+                      console.log("");
+                      connection.end();
+                    }
+                  );
+                }
+              });
+          }
+        }); 
+      });  
+  };
+  
+  display();
